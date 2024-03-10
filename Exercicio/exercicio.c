1ef4288a49bd8e2b11968e86231dd2f8f01985ef
filhoalexandre <filhoalexandre@NewBubunto.myguest.virtualbox.org>
2024-03-10 14:34:50 -0300

    Corrigido pontos para matriz

--- Exercicio/exercicio.c
+++ Exercicio/exercicio.c
@@ -10,23 +10,31 @@ typedef struct {
 
 void desenha();
 void init();
-void draw(char*,ponto*,int);
+void draw(char*,int(*)[2],int);
 
 GLint   WIDTH = 500,
         HEIGHT = 500;
 
-ponto A,B,C,D,Z,X,Y;
 
-ponto quad[4];
-ponto tri[3];
+int quadrado[][2] = {
+    {-1,-1},
+    {1,-1},
+    {1,1},
+    {-1,1}
+};
 
-// ponto triangulo[3] = {A,B,C};
+int triangulo[][2] = {
+    {-1,0},
+    {1,0},
+    {0,1}
+};
+
 int main(int argc, char *argv[])
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowPosition(400 ,100);
-    glutInitWindowSize(WIDTH,WIDTH);
+    glutInitWindowSize(WIDTH,HEIGHT);
     glutCreateWindow("Exercicio - Castelo");
 
     init();
@@ -41,55 +49,27 @@ void init(){
     glClearColor(1.0, 1.0, 1.0, 1.0);
     gluOrtho2D(-2,2,-2,2);
 
-    //Coordenadas do quad
-    A.x = -1;
-    A.y = -1;
-    B.x = 1;
-    B.y = -1;
-    C.x = 1;
-    C.y = 1;
-    D.x = -1;
-    D.y = 1;
-    quad[0] = A;
-    quad[1] = B;
-    quad[2] = C;
-    quad[3] = D;
-
-    //Coordenadas do triangulo
-    Z.x = -1;
-    Z.y = 0;
-    X.x = 1;
-    X.y = 0;
-    Y.x = 0;
-    Y.y = 1;
-    tri[0] = Z;
-    tri[1] = X;
-    tri[2] = Y;
-    
-
 }
 
 void desenha(){
 
     glClear(GL_COLOR_BUFFER_BIT);
     glPointSize(10);
-    draw("vertices",quad,4);
-    draw("linhas",quad,4);
-    draw("vertices",tri,3);
-    draw("linhas",tri,3);
+    draw("vertices",quadrado,4);
+    draw("linhas",quadrado,4);
+    draw("vertices",triangulo,3);
+    draw("linhas",triangulo,3);
     glFlush();
 }
 
-void draw(char *tipo, ponto *figura, int n){
+void draw(char *tipo, int(*figura)[2], int n){
 
-    // char aux[100];
-    // strcpy(tipo,aux);
     if(!strcmp("vertices",tipo)){
         glPointSize(10);
         glColor3f(1.0,0.0,0.0);
         glBegin(GL_POINTS);
             for(int i = 0; i < n; i++){
-                glVertex2i(figura[i].x, figura[i].y);
+                glVertex2i(figura[i][0], figura[i][1]);
             }
         glEnd();
     }
@@ -98,10 +78,10 @@ void draw(char *tipo, ponto *figura, int n){
         glColor3f(1.0,0.0,0.0);
         glBegin(GL_LINES);
             for(int i = 0; i < n; i++){
-                printf("figura[i].x: %d, figura[i].y: %d\n", figura[i].x, figura[i].y);
-                glVertex2i(figura[i].x, figura[i].y);
-                if(i == n-1)glVertex2i(figura[0].x, figura[0].y);
-                else glVertex2i(figura[i+1].x, figura[i+1].y);
+                printf("figura[i][0]: %d, figura[i][1]: %d\n", figura[i][0], figura[i][1]);
+                glVertex2i(figura[i][0], figura[i][1]);
+                if(i == n-1)glVertex2i(figura[0][0], figura[0][1]);
+                else glVertex2i(figura[i+1][0], figura[i+1][1]);
             }
         glEnd();
     }
