3e3557b036c1b1977ca14b78643d1b88b547b1d9
filho.alexandre <filho.alexandre@graduacao.uerj.br>
2024-04-10 00:06:25 -0300

    Mesa

---
+++ Mesa/mesa.c
@@ -0,0 +1,88 @@
+#include <GL/glut.h>
+
+
+void display() {
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+  glMatrixMode(GL_MODELVIEW);
+  glLoadIdentity();
+
+
+
+  glEnable(GL_LIGHT0);
+  glEnable(GL_LIGHT1);
+
+  glPushMatrix();
+  glRotated(10, 1.0, 0.0, 0.0);
+  //glTranslatef(1, 0.0, -3.0);
+  glScaled(5.0, 1.0, 3.0);
+  glutSolidCube(1.0);
+  glPopMatrix();
+
+  glDisable(GL_LIGHT0);
+  glDisable(GL_LIGHT1);
+
+
+
+  glFlush();
+}
+
+
+
+
+
+void init() {
+
+  glMatrixMode(GL_PROJECTION);
+  glLoadIdentity();
+
+  glOrtho(-10.0, 10.0, -10.0, 10.0, 10.0, -10.0);
+
+
+
+
+
+  GLfloat black[] = { 0.0, 0.0, 0.0, 1.0 };
+  GLfloat pink[] = { 1.0, 0.5, 1.0, 1.0 };
+  GLfloat pink2[] = { 0.5, 1.0, 0.5, 1.0 };
+  GLfloat yellow[] = { 1.0, 1.0, 0.0, 1.0 };
+  GLfloat cyan[] = { 0.0, 1.0, 1.0, 1.0 };
+  GLfloat white[] = { 1.0, 1.0, 1.0, 1.0 };
+  GLfloat direction[] = { 0.0, 0.0, 1.0, 1.0 };
+  GLfloat direction1[] = { 0.0,0.0, 1.0, 1.0 };
+
+  glMaterialfv(GL_FRONT, GL_AMBIENT_AND_DIFFUSE, cyan);
+  glMaterialfv(GL_FRONT, GL_SPECULAR, white);
+  glMaterialf(GL_FRONT, GL_SHININESS, 50);
+
+
+
+  glLightfv(GL_LIGHT0, GL_AMBIENT, black);
+  glLightfv(GL_LIGHT0, GL_DIFFUSE, yellow);
+  glLightfv(GL_LIGHT0, GL_SPECULAR, white);
+  glLightfv(GL_LIGHT0, GL_POSITION, direction);
+
+  glLightfv(GL_LIGHT1, GL_AMBIENT, black);
+  glLightfv(GL_LIGHT1, GL_DIFFUSE, white);
+  glLightfv(GL_LIGHT1, GL_SPECULAR, white);
+  glLightfv(GL_LIGHT1, GL_POSITION, direction1);
+
+  glEnable(GL_LIGHTING);                // so the renderer considers light
+  glEnable(GL_LIGHT0);                  // turn LIGHT0 on
+  glEnable(GL_LIGHT1);
+  glEnable(GL_DEPTH_TEST);              // so the renderer considers depth
+
+
+}
+
+
+int main(int argc, char** argv) {
+  glutInit(&argc, argv);
+  glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB | GLUT_DEPTH);
+  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(600, 600);
+  glutCreateWindow("Mesa");
+  glutDisplayFunc(display);
+  init();
+  glutMainLoop();
+}
